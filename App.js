4ad91b4a26b0d3f8c9ff1c2951c136d5c8879643
Triston Herbst <tristonherbst97@gmail.com>
2021-10-04 14:39:14 -0500

    register screen

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 
 export default function App() {
-  return <LoginScreen />;
+  return <RegisterScreen />;
 }
