857660c5137a6e57d85b1f2098bea84f9f5d9485
Triston Herbst <tristonherbst97@gmail.com>
2021-10-06 10:58:55 -0500

    custom width for text input and picker

--- components/forms/AppFormField.js
+++ components/forms/AppFormField.js
@@ -4,14 +4,13 @@ import { useFormikContext } from "formik";
 import AppTextInput from "../AppTextInput";
 import ErrorMessage from "./ErrorMessage";
 
-export default function AppFormField({ name, width, ...otherProps }) {
+export default function AppFormField({ name, ...otherProps }) {
   const { setFieldTouched, errors, handleChange, touched } = useFormikContext();
   return (
     <>
       <AppTextInput
         onChangeText={handleChange(name)}
         onBlur={() => setFieldTouched(name)}
-        width={width}
         {...otherProps}
       />
       <ErrorMessage error={errors[name]} visible={touched[name]} />
